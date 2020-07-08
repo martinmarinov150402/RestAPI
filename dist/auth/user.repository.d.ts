e6d939d97601ec5622d5a9327bc3718f0ea971e1
Martin Marinov <martinmarinov150402@gmail.com>
2020-07-08 13:16:03 +0300

    Fixed Config

--- dist/auth/user.repository.d.ts
+++
@@ -1,9 +0,0 @@
-import { Repository } from "typeorm";
-import { User } from "./user.entity";
-import { AuthCredentialsDTO } from "./dto/auth-credentials.dto";
-export declare class UserRepository extends Repository<User> {
-    signUp(authCredentialsDto: AuthCredentialsDTO): Promise<void>;
-    private hashPassword;
-    signIn(authCredentialsDto: AuthCredentialsDTO): Promise<string>;
-    private validateUserPassword;
-}
