38a47925e29d29119b9f6d7fd851adb66d1b4cbc
Martin Marinov <martinmarinov150402@gmail.com>
2020-07-08 13:38:35 +0300

    Fail Fix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import * as config from 'config';
 
 async function bootstrap() {
-  console.log(process.env.DB_HOST+" "+process.env.DB_PORT+" "+process.env.DB_USERNAME+" "+process.env.DB_PASSWORD);
   const serverConfig = config.get('server');
   const app = await NestFactory.create(AppModule);
 
